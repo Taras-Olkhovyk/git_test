0690184579eda23778dc4a0483a10991f150cbc0
Taras Olkhovyk <olhoviktaras0@gmail.com>
2022-06-05 16:00:07 +0300

    new commit

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
-from blog import create_app, create_user
+from flask import Flask, render_template
 
-app = create_app()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    create_user()
+    
     app.run(debug=True)
     
 
