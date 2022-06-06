e06e2c647046de77cd05efd7938c84089ae6a2fd
Михаил Терехов <47977649+MikesoWeb@users.noreply.github.com>
2022-06-06 19:34:54 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
-from blog import  create_app, db
-
+from blog import create_app, db
 
 app = create_app()
 
-
-
-
-
-if __name__ == "__main__":    
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         app.run(debug=True)
